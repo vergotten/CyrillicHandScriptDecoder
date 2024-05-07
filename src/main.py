15db628395c27534adf93ee0c5b7c65fde5ae300
vergotten <vergotten@gmail.com>
2024-05-07 13:55:43 +0300

    updated main

--- src/main.py
+++ src/main.py
@@ -3,20 +3,15 @@ from config import Hparams
 import argparse
 
 
-def main(hp):
-    # CREATE MAPS FROM CHARACTERS TO INDICIES AND VISA VERSA
-    char2idx = {char: idx for idx, char in enumerate(hp.cyrillic)}
-    idx2char = {idx: char for idx, char in enumerate(hp.cyrillic)}
-
-    return char2idx, idx2char
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", help="Path to JSON configuration file")
     args = parser.parse_args()
 
     hp = Hparams(args.config)
+
+    char2idx = {char: idx for idx, char in enumerate(hp.cyrillic)}
+    idx2char = {idx: char for idx, char in enumerate(hp.cyrillic)}
 
     torch.manual_seed(42)
     torch.cuda.manual_seed(42)
