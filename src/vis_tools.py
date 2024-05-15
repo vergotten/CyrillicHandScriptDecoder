7bbc2be3b0cfde09bf205f5e508676aa533fbc26
vergotten <vergotten@gmail.com>
2024-05-15 05:44:14 +0300

    implemented vis tools

--- src/vis_tools.py
+++ src/vis_tools.py
@@ -1,77 +1,96 @@
-import matplotlib.pyplot as plt
+import torch
 import numpy as np
+import os
+import argparse
+import matplotlib.pyplot as plt
+
+from data.data_processing import process_data, train_valid_split
+from config import Hparams
+from data.collate import TextCollate
+from data.dataset import TransformedTextDataset
+from data.text_utils import labels_to_text
 
 
-def display_images(images, labels, batch_size):
-    """
-    Display a batch of images with their labels.
-
-    Args:
-        images (list of np.array): Batch of images.
-        labels (list of str): Corresponding labels of images.
-        batch_size (int): Size of the batch.
-    """
-    # Calculate the number of rows and columns
-    columns = int(np.sqrt(batch_size)) + 1
-    rows = int(np.ceil(batch_size / columns)) + 1
-
-    # Adjust the figure size based on the number of columns and rows
-    fig = plt.figure(figsize=(columns * 2, rows * 2))
-
-    for i in range(batch_size):
-        ax = fig.add_subplot(rows, columns, i + 1)
-        image = images[i]
-        # Check if the image is not already normalized
-        if image.max() > 1.0:
-            # Divide by 255 to get pixel values between 0 and 1
-            image = image / 255.0
-        plt.imshow(image)
-        # Display the label as the title of the subplot
-        plt.title(f"{labels[i]}")
-        plt.axis('off')  # to hide the axis
-        # Reduce space between rows
-        ax.set_xticks([])
-        ax.set_yticks([])
-    plt.tight_layout(pad=0.5)  # Adjust the layout to minimize overlap and reduce padding
-    plt.show()
-
-
-def display_batch(data_loader, batch_size):
+def visualize_batch(data_loader, idx2char, num_images=10):
     """
     Display a batch of images with their labels.
 
     Args:
         data_loader (DataLoader): PyTorch DataLoader.
-        batch_size (int): Size of the batch.
+        idx2char (dict): Mapping from indices to characters.
+        num_images (int): Number of images to display.
     """
-    # Get a batch of data
-    images, labels = next(iter(data_loader))
+    # Get the first batch
+    first_batch_images, first_batch_labels = next(iter(data_loader))
 
-    # Calculate the number of rows and columns
-    columns = int(np.sqrt(batch_size)) + 1
-    rows = int(np.ceil(batch_size / columns)) + 1
+    # Calculate the number of rows and columns for the subplot grid
+    columns = int(np.sqrt(num_images)) + 1
+    rows = int(np.ceil(num_images / columns)) + 1
 
-    # Adjust the figure size based on the number of columns and rows
+    # Create a figure for the entire grid
     fig = plt.figure(figsize=(columns * 2, rows * 2))
 
-    for i in range(batch_size):
-        ax = fig.add_subplot(rows, columns, i + 1)
-        image = images[i].numpy().transpose((1, 2, 0))  # Convert the image from PyTorch tensor to numpy array
+    # Iterate over each label in the batch
+    for i in range(first_batch_labels.size(1)):
+        # Get the label for the i-th sample in the batch
+        label = first_batch_labels[:, i]
+
+        # Remove trailing zeros (padding)
+        label = label[label != 0]
+
+        # Convert the label to text and print it
+        print(f"Label {i}: {labels_to_text(label.tolist(), idx2char)}")
+
+        # Visualize the i-th image in the batch
+        image = first_batch_images[i].numpy().transpose((1, 2, 0))  # Convert the image from PyTorch tensor to numpy array
         # Check if the image is not already normalized
         if image.max() > 1.0:
             # Divide by 255 to get pixel values between 0 and 1
             image = image / 255.0
+
+        # Add the image to the subplot
+        ax = fig.add_subplot(rows, columns, i + 1)
         plt.imshow(image)
-        # Display the label as the title of the subplot
-        plt.title(f"Label: {labels[i]}")
-        plt.axis('off')  # to hide the axis
-        # Reduce space between rows
-        ax.set_xticks([])
-        ax.set_yticks([])
+        plt.title(f"{labels_to_text(label.tolist(), idx2char)}")
+        plt.axis('on')  # to hide the axis
+
+    # Show the entire grid
     plt.tight_layout(pad=0.1)  # Adjust the layout to minimize overlap and reduce padding
     plt.show()
 
 
-# Display a batch from the train_loader and val_loader
-# display_batch(train_loader, batch_size=18)
-# display_batch(val_loader, batch_size=18)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='OCR Training')
+    parser.add_argument("--config", default="configs/config.json", help="Path to JSON configuration file")
+    parser.add_argument('--train_data', type=str, default='data/train/', help='Path to training data')
+    parser.add_argument('--test_data', type=str, default='data/test/', help='Path to testing data')
+    parser.add_argument('--train_labels', type=str, default='data/train.tsv', help='Path to training labels')
+    parser.add_argument('--test_labels', type=str, default='data/test.tsv', help='Path to testing labels')
+
+    args = parser.parse_args()
+
+    args.train_data = os.path.abspath(args.train_data)
+    args.test_data = os.path.abspath(args.test_data)
+    args.train_labels = os.path.abspath(args.train_labels)
+    args.test_labels = os.path.abspath(args.test_labels)
+
+    hp = Hparams(args.config)
+    print(vars(hp))
+
+    char2idx = {char: idx for idx, char in enumerate(hp.cyrillic)}
+    idx2char = {idx: char for idx, char in enumerate(hp.cyrillic)}
+
+    img2label, _, all_words = process_data(args.train_data, args.train_labels)
+
+    X_val, y_val, X_train, y_train = train_valid_split(img2label, train_part=0.01, val_part=0.001)
+
+    train_dataset = TransformedTextDataset(X_train, y_train, char2idx, idx2char, hp, train=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=False,
+                                               batch_size=hp.batch_size, pin_memory=True,
+                                               drop_last=True, collate_fn=TextCollate())
+    val_dataset = TransformedTextDataset(X_val, y_val, char2idx, idx2char, hp, train=False)
+    val_loader = torch.utils.data.DataLoader(val_dataset, shuffle=False,
+                                             batch_size=hp.batch_size, pin_memory=False,
+                                             drop_last=False, collate_fn=TextCollate())
+
+    visualize_batch(val_loader, idx2char, num_images=hp.batch_size)
