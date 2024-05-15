0d173a8f18da828cb0ddeb8e436a532e834c0920
vergotten <vergotten@gmail.com>
2024-05-15 06:07:57 +0300

    updated model

--- src/model.py
+++ src/model.py
@@ -127,10 +127,10 @@ if __name__ == "__main__":
         "dropout": 0.0,
         "width": 256,
         "height": 64,
-        "path_test_dir": "data/test/",
-        "path_test_labels": "data/test.tsv",
-        "path_train_dir": "data/train/",
-        "path_train_labels": "data/train.tsv"
+        "path_test_dir": "utils/test/",
+        "path_test_labels": "utils/test.tsv",
+        "path_train_dir": "utils/train/",
+        "path_train_labels": "utils/train.tsv"
     }
 
     # Define the model using the configuration
