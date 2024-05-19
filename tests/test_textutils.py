6aabac699903172e1528828b2634f8b753e9a657
vergotten <vergotten@gmail.com>
2024-05-19 18:51:24 +0300

    Updated test textutils

--- tests/test_textutils.py
+++ tests/test_textutils.py
@@ -8,11 +8,15 @@ class TestTextUtils(unittest.TestCase):
 
     def setUp(self):
         # Define character to index and index to character mappings
-        self.cyrillic = ['PAD', 'SOS', ' ', '!', '"', '%', '(', ')', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', '[', ']', '«', '»', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'ё', 'EOS']
+        self.cyrillic = ['PAD', 'SOS', ' ', '!', '"', '%', '(', ')', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6',
+                    '7', '8', '9', ':', ';', '?', '[', ']', '«', '»', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й',
+                    'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а',
+                    'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+                    'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'ё', 'EOS']
         self.char2idx = {char: idx for idx, char in enumerate(self.cyrillic)}
         self.idx2char = {idx: char for idx, char in enumerate(self.cyrillic)}
 
-        # For the purpose of this example, let's use a subset of your Cyrillic characters
+        # For the purpose of this example, let's use a subset of Cyrillic characters
         self.truth = 'АБВГДЕЖЗ'
         self.pred = 'ЗЖЕДВБА'
 
@@ -36,6 +40,32 @@ class TestTextUtils(unittest.TestCase):
     def test_word_error_rate(self):
         expected_wer = lev.distance(' '.join(self.truth.split()), ' '.join(self.pred.split())) / len(self.truth.split())
         wer = word_error_rate(self.truth, self.pred)
+        self.assertEqual(wer, expected_wer)
+
+    def test_labels_to_text_with_special_characters(self):
+        indices = [1, 3, 4, 5, 6, 7, 8, 9, 10, 2]  # Corrected indices
+        expected_output = '!"%(),-.'
+        output = labels_to_text(indices, self.idx2char)
+        self.assertEqual(output, expected_output)
+
+    def test_text_to_labels_with_special_characters(self):
+        text = '!\"%()-.,'
+        expected_output = [1, 3, 4, 5, 6, 7, 9, 10, 8, 91]
+        output = text_to_labels(text, self.char2idx)
+        self.assertEqual(output, expected_output)
+
+    def test_char_error_rate_with_different_lengths(self):
+        truth = 'АБВГДЕЖЗ'
+        pred = 'ЗЖЕДВ'
+        expected_cer = lev.distance(truth, pred) / len(truth)
+        cer = char_error_rate(truth, pred)
+        self.assertEqual(cer, expected_cer)
+
+    def test_word_error_rate_with_multiple_words(self):
+        truth = 'АБ ВГ ДЕ ЖЗ'
+        pred = 'ЗЖ ЕД ВБ А'
+        expected_wer = lev.distance(' '.join(truth.split()), ' '.join(pred.split())) / len(truth.split())
+        wer = word_error_rate(truth, pred)
         self.assertEqual(wer, expected_wer)
 
 
