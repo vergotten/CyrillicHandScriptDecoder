70c2d88eaefea4fefa167258da79c818e0f7bb8f
vergotten <vergotten@gmail.com>
2024-05-19 18:10:01 +0300

    Updated test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,24 +1,33 @@
 import unittest
 import json
 import os
+from src.config import Hparams
 
 
-class TestConfig(unittest.TestCase):
+class TestHparams(unittest.TestCase):
+
     def setUp(self):
-        # Load the configuration file
-        with open('configs/config.json', 'r') as f:
-            self.config = json.load(f)
+        self.config_path = 'configs/config.json'
+        self.hparams = Hparams(self.config_path)
+
+    def test_hparams_initialization(self):
+        # Check if the Hparams object was initialized correctly
+        self.assertIsInstance(self.hparams, Hparams)
 
     def test_config_loading(self):
         # Check if the configuration file was loaded correctly
-        self.assertIsInstance(self.config, dict)
+        with open(self.config_path, 'r') as f:
+            config = json.load(f)
+        hparams_dict = vars(self.hparams).copy()
+        hparams_dict.pop("cyrillic")  # Don't compare the 'cyrillic' attribute
+        self.assertDictEqual(hparams_dict, config)
 
     def test_paths(self):
         # Check if the paths in the configuration file exist
-        self.assertTrue(os.path.exists(self.config['path_test_dir']))
-        self.assertTrue(os.path.exists(self.config['path_test_labels']))
-        self.assertTrue(os.path.exists(self.config['path_train_dir']))
-        self.assertTrue(os.path.exists(self.config['path_train_labels']))
+        self.assertTrue(os.path.exists(self.hparams.path_test_dir))
+        self.assertTrue(os.path.exists(self.hparams.path_test_labels))
+        self.assertTrue(os.path.exists(self.hparams.path_train_dir))
+        self.assertTrue(os.path.exists(self.hparams.path_train_labels))
 
 
 if __name__ == '__main__':
